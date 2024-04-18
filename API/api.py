1e189eac1ba469417600ee680a32d70a057607ac
Amaan Jamadar <amaanjamadar8@gmail.com>
2024-04-18 19:41:24 +0530

    update the api

--- API/api.py
+++ API/api.py
@@ -130,6 +130,51 @@ async def predict(file: UploadFile):
             'ismedicinal':is_medicinal,
             'details':details
             }
+@app.post("/fx/")
+async def get_features(file: UploadFile):
+    img = iamge_ndarray(await file.read())
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+    green_lower = (36, 0, 0)
+    green_upper = (86, 255, 255)
+    brown_lower = (8, 60, 20)
+    brown_upper = (30, 255, 200)
+    yellow_lower = (21, 39, 64)
+    yellow_upper = (40, 255, 255)
+
+    mask_green = cv.inRange(hsv, green_lower, green_upper)
+    mask_brown = cv.inRange(hsv, brown_lower, brown_upper)
+    mask_yellow = cv.inRange(hsv, yellow_lower, yellow_upper)
+    mask_combined = cv.bitwise_or(mask_green, mask_brown)
+    mask_combined = cv.bitwise_or(mask_combined, mask_yellow)
+
+    result = cv.bitwise_and(img, img, mask=mask_combined)
+    contours, _ = cv.findContours(mask_combined, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    x, y, w, h = cv.boundingRect(max(contours, key=cv.contourArea))
+
+    zoomed_in_img = result[y:y+h, x:x+w]
+    zoomed_in_img_blurred = cv.GaussianBlur(zoomed_in_img, (5, 5), 0)
+    zoomed_in_img_rgb = cv.cvtColor(zoomed_in_img_blurred, cv.COLOR_BGR2RGB)
+    zoomed_in_img_gray = cv.cvtColor(zoomed_in_img_rgb, cv.COLOR_BGR2GRAY)
+
+    edges_noraml = cv.Canny(zoomed_in_img_gray, 100, 200)
+    zoomed_in_img_gray = cv.cvtColor(zoomed_in_img_blurred, cv.COLOR_BGR2GRAY)
+    clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    enhanced_img = clahe.apply(zoomed_in_img_gray)
+    edges = cv.Canny(enhanced_img, 100, 200)
+
+    _, img_bytes = cv.imencode('.png', zoomed_in_img_rgb)
+    _, enhanced_bytes = cv.imencode('.png', enhanced_img)
+    _, edges_bytes = cv.imencode('.png', edges)
+    _, edges_normal_bytes = cv.imencode('.png', edges_normal)
+
+    return Response(content=img_bytes.tobytes(), media_type="image/png"), \
+           Response(content=enhanced_bytes.tobytes(), media_type="image/png"), \
+           Response(content=edges_bytes.tobytes(), media_type="image/png"), \
+           Response(content=edges_normal_bytes.tobytes(), media_type="image/png")
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app,host='localhost',port=8000)
